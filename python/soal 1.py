3b3000dcad3a19c69fa78efdecc5c8713caea6b4
= <=>
2024-10-29 09:13:37 +0700

    soal 1

---
+++ python/soal 1.py
@@ -0,0 +1,7 @@
+# Menampilkan output sesuai format yang diinginkan
+
+print("******************************************")
+print("             PROGRAM PYTHON")
+print("             OLEH: Alifka areiga")
+print("             KELAS: X PPLG 1")
+print("******************************************")
